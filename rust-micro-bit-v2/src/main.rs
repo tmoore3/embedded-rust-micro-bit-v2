68e8880c6bb2daf777f740537910f9689d79c34c
Tom Moore <tomwmoore@hotmail.com>
2024-03-30 22:04:08 +0000

    Renamed project from "embedded-rust-micro-bit-v2" to "rust-micro-bit-v2".

--- rust-micro-bit-v2/src/main.rs
+++ rust-micro-bit-v2/src/main.rs
@@ -3,13 +3,14 @@
 
 use cortex_m::asm::nop;
 use cortex_m_rt::entry;
-use  panic_halt as _;
+use panic_halt as _;
 use rtt_target::{rprintln, rtt_init_print};
 
 #[entry]
 fn main() -> ! {
     rtt_init_print!();
     rprintln!("Hello World!");
+    // let mut x: usize = 0;
     loop {
         rprintln!("Echo...");
         for _ in 0..100_000 {
